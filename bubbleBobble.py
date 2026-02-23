8f96beef79ace2d2d5b83235bcf49d5f2d39f746
desguerra728 <desguerra728@phm.k12.in.us>
2026-02-23 08:44:46 -0500

    write basic desc

---
+++ bubbleBobble.py
@@ -0,0 +1,18 @@
+''']
+3 lives
+platform: player can jump up on them, cannot fall through them
+enemies fall from top to the bottom, then move towards player
+    same platform restrictions as player
+    "angry mode"
+shoot bubbles
+    can jump over bubbles
+catch enemies in bubble
+    enemy in bubble floats up
+    when enemy in bubble and player touches, enemy dies
+    when enemy dead, drop food
+    collect food, gain points
+    higher points scores when bursting several bubbles at the same time
+if touch enemy and not in bubble, dead
+
+
+'''
